d255ac0d558ad716b06d71e738373413c611b955
maher-cs <maher.azkoul@gmail.com>
2019-12-14 14:26:45 +0300

    assignments: assignment-03: part-1-cross-one-lane-bridge-v1

---
+++ assignments/assignment-03/part-1-crossing-one-lane-bridge/Bridge.java
@@ -0,0 +1,128 @@
+/*
+ * UQU - CS - Operating Systems
+ * pr. Abdulbaset Gaddah
+ * subject: assignment-03-part-1: crossing-one-lane-bridge
+ * authors:
+ * - MHD Maher Azkoul
+ *   438017578
+ *   group: 3 - no.: 24
+ * 
+ * program description:
+ *    This program simulates a traffic senario where 
+ * we need a semaphores to control the traffic.
+ *    The program simulates two bounds connected with a bridge
+ * that have only one lane
+ *    This is a part of 6 parts of the whole program.
+ * This part simulates the one-lane-bridge that is connecting
+ * the two bounds.
+ *    This part uses semaphores to control traffic and avoid
+ * deadlock and starvation
+ * 
+ */
+
+import java.util.concurrent.Semaphore;
+
+public class Bridge {
+
+    // semaphore
+    private Semaphore semMutex;
+
+    // reporter stuff
+    private int eastCounter;
+    private int westCounter;
+
+    // default constructor
+    public Bridge() {
+        this.semMutex = new Semaphore(1);
+        this.reportThread();
+    }
+
+    // crossing bridge by vehicle simulation method
+    public void cross(Vehicle vehicle) {
+        try {
+            // string variable that holds the direction and id of the vehicle
+            String identity = vehicle.getDirection() + " vehicle: " + vehicle.getVehicleId() + " ";
+
+            // message that tell the user that there is a vehicle
+            // waiting for its turn to cross the bridge
+            System.out.println(identity + "is WAITING to cross the bridge.");
+
+            // entry setion, only one vehicle at a the 
+            // same time can go after this line
+            semMutex.acquire();
+
+            // start critical section
+
+            // start crossing the bridge
+            System.out.println(identity + "is CROSSING the bridge now.");
+            vehicle.crossBridge();
+            System.out.println(identity + "has COMPLETED crossing the bridge.");
+
+            // count the car that crossed the bridge
+            // the reporter will use counters in the report
+            this.increaseCounters(vehicle);
+            // end critical section
+
+            // tell the bridge that the vehicle
+            // is leaving and the bridge is ready 
+            // to receive a new vehilce
+            semMutex.release();
+        } catch (Exception e) {
+            System.err.println(e + ": Bridge: cross");
+        }
+
+    }
+
+    // reporter method that create a reporter thread
+    // that report the number of vehicles that have 
+    // crossed the bridge for last period of time
+    // The reporter thread give a report every while
+    private void reportThread() {
+        this.initCounters();
+
+        final int REPORT_INTERVAL = 30 * 1000;
+
+        Thread reporter = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        semMutex.acquire();
+
+                        System.out.println("Number of eastbound vehicles passed the bridge: " + eastCounter);
+                        System.out.println("Number of westbound vehicles passed the bridge: " + westCounter);
+                        initCounters();
+
+                        semMutex.release();
+                        sleep(REPORT_INTERVAL);
+                    }
+                } catch (Exception e) {
+                    System.err.println(e + ": Bridge: reporter: run");
+                }
+            };
+        };
+
+        reporter.start();
+
+    }
+
+    // ## reproter helpers private methods ##
+
+    // set counters to initial values
+    private void initCounters() {
+        this.eastCounter = 0;
+        this.westCounter = 0;
+    }
+
+    // increase counter for each vehicle
+    private void increaseCounters(Vehicle vehicle) {
+        switch (vehicle.getDirection()) {
+        case "Eastbound":
+            this.eastCounter++;
+            break;
+        case "Westbound":
+            this.westCounter++;
+            break;
+        }
+    }
+}
---
+++ assignments/assignment-03/part-1-crossing-one-lane-bridge/WestboundThread.java
@@ -0,0 +1,30 @@
+/*
+ * UQU - CS - Operating Systems
+ * pr. Abdulbaset Gaddah
+ * subject: assignment-03-part-1: crossing-one-lane-bridge
+ * authors:
+ * - MHD Maher Azkoul
+ *   438017578
+ *   group: 3 - no.: 24
+ * 
+ * program description:
+ *    This program simulates a traffic senario where 
+ * we need a semaphores to control the traffic.
+ *    The program simulates two bounds connected with a bridge
+ * that have only one lane
+ *    This is a part of 6 parts of the whole program.
+ * This part simulates the east-bound.
+ *    This part extends the bound class and define 
+ * the west direction as its direction.
+ * 
+ */
+
+
+public class WestboundThread extends BoundThread {
+
+    public WestboundThread(Bridge bridge) {
+        super(bridge);
+        this.direction = "Westbound";
+    }
+
+}
